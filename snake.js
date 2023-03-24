32bacfddf460419e847de2f710e0e5c51869b733
Gnuff <Gnuff@users.noreply.github.com>
2023-03-24 23:56:07 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -67,3 +67,10 @@ function loop() {
 }
 
 loop();
+
+window.addEventListener('keydown', (event) => {
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
+        event.preventDefault();
+    }
+});
+
