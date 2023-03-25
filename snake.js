f318eaab30388a7626245a40b3b42d5a10e853ae
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 02:21:25 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,175 +1,99 @@
-const canvas = document.getElementById('game-board');
-const ctx = canvas.getContext('2d');
-
-const resolution = 20;
-canvas.width = 400;
-canvas.height = 400;
-
-class Snake {
-    constructor() {
-        this.snakeArray = [
-            { x: 10 * resolution, y: 10 * resolution }
-        ];
-        this.direction = 'Right';
-        this.newDirection = 'Right';
-        this.growSnake = false;
-    }
-
-    updateDirection(newDirection) {
-        this.newDirection = newDirection;
-    }
-
-    move() {
-        const head = { ...this.snakeArray[0] };
-
-        if (this.direction === 'Up') head.y -= resolution;
-        if (this.direction === 'Down') head.y += resolution;
-        if (this.direction === 'Left') head.x -= resolution;
-        if (this.direction === 'Right') head.x += resolution;
-
-        if (this.growSnake) {
-            this.growSnake = false;
-        } else {
-            this.snakeArray.pop();
-        }
-
-        this.snakeArray.unshift(head);
-        this.direction = this.newDirection;
-    }
-
-    draw() {
-        ctx.fillStyle = '#34854b';
-        for (const cell of this.snakeArray) {
-            ctx.fillRect(cell.x, cell.y, resolution, resolution);
-        }
-    }
-
-    collidedWithWall() {
-        const head = this.snakeArray[0];
-        return (
-            head.x < 0 ||
-            head.y < 0 ||
-            head.x >= canvas.width ||
-            head.y >= canvas.height
-        );
-    }
-
-    collidedWithItself() {
-        const head = this.snakeArray[0];
-        for (const cell of this.snakeArray.slice(1)) {
-            if (head.x === cell.x && head.y === cell.y) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    reset() {
-        this.snakeArray = [
-            { x: 10 * resolution, y: 10 * resolution }
-        ];
-        this.direction = 'Right';
-        this.newDirection = 'Right';
-    }
-
-    eatApple(apple) {
-        if (
-            this.snakeArray[0].x === apple.position.x &&
-            this.snakeArray[0].y === apple.position.y
-        ) {
-            this.growSnake = true;
-            return true;
-        }
-        return false;
-    }
-}
-
-class Apple {
-    constructor(snake) {
-        this.position = { x: 0, y: 0 };
-        this.generate(snake);
-    }
-
-    generate(snake) {
-        let newPosition;
-
-        do {
-            newPosition = {
-                x: Math.floor(Math.random() * (canvas.width / resolution)) * resolution,
-                y: Math.floor(Math.random() * (canvas.height / resolution)) * resolution
-            };
-        } while (snake.snakeArray.some(cell => cell.x === newPosition.x && cell.y === newPosition.y));
-
-        this.position = newPosition;
-    }
-
-    draw() {
-        ctx.fillStyle = '#e63946';
-        ctx.fillRect(this.position.x, this.position.y, resolution, resolution);
-    }
-}
-
-// ... previous code ...
-
-const snake = new Snake();
-const apple = new Apple(snake);
-let score = 0;
+const canvas = document.getElementById("game-board");
+const ctx = canvas.getContext("2d");
+const gridSize = 20;
+const tileCount = canvas.width / gridSize;
+const delay = 100;
+let snake = [];
+let direction = { x: 1, y: 0 };
+let apple = {};
+let currentScore = 0;
 let topScore = 0;
-const currentScoreElement = document.getElementById('current-score');
-const topScoreElement = document.getElementById('top-score');
-let deathCounter = 0;
-const deathCounterElement = document.getElementById('death-counter');
+let deathCount = 0;
+let gameInterval;
 
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    snake.draw();
-    apple.draw();
-
-    snake.move();
-
-    if (snake.collidedWithWall() || snake.collidedWithItself()) {
-        snake.reset();
-        apple.generate(snake);
-        score = 0;
-        currentScoreElement.textContent = `Current score: ${score}`;
-        // Increment the death counter and update the display
-        deathCounter++;
-        deathCounterElement.textContent = `Deaths: ${deathCounter}`;
-    }
-
-    if (snake.eatApple(apple)) {
-        apple.generate(snake);
-        score++;
-        currentScoreElement.textContent = `Current score: ${score}`;
-
-        if (score > topScore) {
-            topScore = score;
-            topScoreElement.textContent = `Top score: ${topScore}`;
-        }
-    }
-
-    setTimeout(gameLoop, 150);
+function init() {
+    snake = [
+        { x: 10, y: 10 },
+        { x: 9, y: 10 },
+        { x: 8, y: 10 },
+    ];
+    direction = { x: 1, y: 0 };
+    apple = spawnApple();
+    currentScore = 0;
+    updateScore();
+    clearInterval(gameInterval);
+    gameInterval = setInterval(game, delay);
 }
 
-gameLoop();
+function game() {
+    updateSnake();
+    checkCollision();
+    draw();
+}
 
-window.addEventListener('keydown', (event) => {
-    const direction = event.key.replace('Arrow', '');
-    const opposites = {
-        Up: 'Down',
-        Down: 'Up',
-        Left: 'Right',
-        Right: 'Left'
+function updateSnake() {
+    const newX = snake[0].x + direction.x;
+    const newY = snake[0].y + direction.y;
+    snake.unshift({ x: newX, y: newY });
+    snake.pop();
+}
+
+function checkCollision() {
+    // Check collision with the wall
+    if (snake[0].x < 0 || snake[0].x >= canvas.width / gridSize || snake[0].y < 0 || snake[0].y >= canvas.height / gridSize) {
+        deathCount++;
+        updateDeathCount();
+        init();
+    }
+}
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw snake
+    ctx.fillStyle = "#50c878";
+    snake.forEach(segment => {
+        ctx.fillRect(segment.x * gridSize, segment.y * gridSize, gridSize - 1, gridSize - 1);
+    });
+
+    // Draw apple
+    ctx.fillStyle = "red";
+    ctx.fillRect(apple.x * gridSize, apple.y * gridSize, gridSize - 1, gridSize - 1);
+}
+
+function spawnApple() {
+    return {
+        x: Math.floor(Math.random() * tileCount),
+        y: Math.floor(Math.random() * tileCount),
+    };
+}
+
+function updateScore() {
+    const currentScoreElement = document.getElementById("current-score");
+    const topScoreElement = document.getElementById("top-score");
+    currentScoreElement.textContent = `Current score: ${currentScore}`;
+    topScoreElement.textContent = `Top score: ${topScore}`;
+}
+
+function updateDeathCount() {
+    const deathCountElement = document.getElementById("death-count");
+    deathCountElement.textContent = `Deaths: ${deathCount}`;
+}
+
+document.addEventListener("keydown", event => {
+    const keyToDirection = {
+        ArrowUp: { x: 0, y: -1 },
+        ArrowDown: { x: 0, y: 1 },
+        ArrowLeft: { x: -1, y: 0 },
+        ArrowRight: { x: 1, y: 0 },
     };
 
-    if (Object.keys(opposites).includes(direction) && direction !== opposites[snake.direction]) {
-        snake.updateDirection(direction);
+    const newDirection = keyToDirection[event.key];
+
+    if (newDirection) {
+        direction = newDirection;
     }
 });
 
-window.addEventListener('keydown', (event) => {
-    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.key)) {
-        event.preventDefault();
-    }
-});
+init();
