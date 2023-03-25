f7bde1b3abc8e6e3efb9e8456d41d93d3296848a
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 11:07:10 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -1,53 +1,45 @@
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-const notes = [  261.63, 293.66, 329.63, 392, 440, 493.88, 523.25, 587.33, 659.25];
-
+const notes = [261.63, 293.66, 329.63, 392, 440, 493.88, 523.25, 587.33, 659.25];
 let startTime = null;
 let scheduledNotes = [];
 let timeoutId = null;
+let isPlaying = false;
 
 function scheduleNote(note, time) {
   const osc = audioContext.createOscillator();
   osc.type = 'sine';
   osc.frequency.value = note;
-
   const gain = audioContext.createGain();
   osc.connect(gain);
   gain.connect(audioContext.destination);
-
   const duration = 0.5;
   gain.gain.setValueAtTime(0.4, time);
   gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
-
   osc.start(time);
   osc.stop(time + duration);
-
   scheduledNotes.push({ osc, time, duration });
 }
 
 function playNotes() {
   const speedFactor = 1 - Math.min(currentScore / 25, 0.7);
   const noteDuration = 0.5 * speedFactor;
-
   notes.forEach((note, index) => {
     const time = startTime + index * noteDuration;
     scheduleNote(note, time);
   });
-
   startTime += notes.length * noteDuration;
 }
 
 function loop() {
-  if (currentScore >= 2) {
-    if (startTime === null) {
-      startTime = audioContext.currentTime + 0.1;
-      playNotes();
-    }
-
-    timeoutId = setTimeout(loop, (notes.length * 0.5 - 0.1) * 1000);
-  } else {
+  if (currentScore >= 2 && !isPlaying) {
+    isPlaying = true;
+    startTime = audioContext.currentTime + 0.1;
+    playNotes();
+  } else if (currentScore < 2 && isPlaying) {
+    isPlaying = false;
     stopMusic();
   }
+  timeoutId = setTimeout(loop, (notes.length * 0.5 - 0.1) * 1000);
 }
 
 function stopMusic() {
@@ -62,3 +54,5 @@ function stopMusic() {
 window.addEventListener('resetMusic', () => {
   stopMusic();
 });
+
+loop();
