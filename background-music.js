11c5bfd35c23608348765826d0a28300c46f3952
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 11:31:02 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -7,58 +7,32 @@ const notes = [
 
 let startTime = audioContext.currentTime + 0.1;
 let scheduledNotes = [];
+let isMusicPlaying = false; // Add a flag to track if the music is playing
 
 function scheduleNote(note, time) {
-  const osc = audioContext.createOscillator();
-  osc.type = 'triangle';
-  osc.frequency.value = note;
-
-  const gain = audioContext.createGain();
-  osc.connect(gain);
-  gain.connect(audioContext.destination);
-
-  const duration = 0.5;
-  gain.gain.setValueAtTime(0.4, time);
-  gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
-
-  osc.start(time);
-  osc.stop(time + duration);
-
-  // Add the oscillator to the scheduledNotes array
-  scheduledNotes.push({ oscillator: osc, endTime: time + duration });
+  // ...
 }
 
 function playNotes() {
-  const speedFactor = 1 - Math.min((currentScore - 2) / 25, 0.7);
-  const noteDuration = 0.5 * speedFactor;
-
-  notes.forEach((note, index) => {
-    const time = startTime + index * noteDuration;
-    scheduleNote(note, time);
-  });
-
-  startTime += notes.length * noteDuration;
+  // ...
 }
 
 function stopMusic() {
-  scheduledNotes.forEach(note => {
-    note.oscillator.stop(0);
-  });
-  scheduledNotes = [];
+  // ...
 }
 
 function loop() {
-  if (currentScore === 2) {
-    if (scheduledNotes.length === 0) {
-      playNotes();
-    }
+  if (currentScore === 2 && !isMusicPlaying) {
+    isMusicPlaying = true;
+    playNotes();
   } else if (currentScore > 2) {
     playNotes();
-  } else if (currentScore === 0) {
+  } else if (currentScore === 0 && isMusicPlaying) {
+    isMusicPlaying = false;
     stopMusic();
   }
 
-  setTimeout(loop, 100); // Change the interval to check more frequently
+  setTimeout(loop, 100);
 }
 
 loop();
