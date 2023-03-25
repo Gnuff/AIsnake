d96ca5293d45bd51010e77c63553683766ba7451
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 08:04:07 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -1,54 +1,65 @@
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-function createOscillator(frequency, type) {
-  const oscillator = audioContext.createOscillator();
-  oscillator.type = type || 'sine';
-  oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
-  return oscillator;
+async function loadDrumSample() {
+  const response = await fetch('https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/22[kb]909bd.aif.mp3');
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  return audioBuffer;
 }
 
-function createGainNode() {
-  const gainNode = audioContext.createGain();
-  gainNode.gain.setValueAtTime(0, audioContext.currentTime);
-  return gainNode;
+function playDrumSample(audioBuffer, time) {
+  const drumSource = audioContext.createBufferSource();
+  drumSource.buffer = audioBuffer;
+  drumSource.connect(audioContext.destination);
+  drumSource.start(time);
 }
 
-function playNote(oscillator, gainNode, startTime, duration) {
-  gainNode.gain.setValueAtTime(0, startTime);
-  gainNode.gain.linearRampToValueAtTime(0.5, startTime + 0.01);
-  gainNode.gain.linearRampToValueAtTime(0, startTime + duration - 0.01);
-  oscillator.start(startTime);
-  oscillator.stop(startTime + duration);
-}
-
-function playMelody() {
+(async () => {
+  const drumBuffer = await loadDrumSample();
   const notes = [
     110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
     174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61,
     130.81, 164.81, 196, 207.65, 196, 164.81, 130.81,
     207.65, 246.94, 293.66, 311.13, 293.66, 246.94, 207.65
   ];
-  const baseDuration = 0.3; // Base duration when the current score is 0
-  const minDuration = 0.1; // Minimum duration when the current score is higher
-  const speedFactor = 1 - Math.min(currentScore / 20, 0.9); // Calculate the speed factor based on the current score
-  const noteDuration = baseDuration * speedFactor; // Calculate the new duration
-  const startTime = audioContext.currentTime;
 
-  notes.forEach((note, index) => {
-    const oscillator = createOscillator(note);
-    const gainNode = createGainNode();
+  let startTime = audioContext.currentTime + 0.1;
 
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
+  function scheduleNote(note, time) {
+    const osc = audioContext.createOscillator();
+    osc.type = 'triangle';
+    osc.frequency.value = note;
 
-    const noteStartTime = startTime + noteDuration * index;
-    playNote(oscillator, gainNode, noteStartTime, noteDuration);
-  });
+    const gain = audioContext.createGain();
+    osc.connect(gain);
+    gain.connect(audioContext.destination);
 
-  setTimeout(playMelody, notes.length * noteDuration * 1000);
-}
+    const duration = 0.5;
+    gain.gain.setValueAtTime(0.4, time);
+    gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
 
-init(); // Call the init function to make sure currentScore is defined
-playMelody();
+    osc.start(time);
+    osc.stop(time + duration);
 
+    playDrumSample(drumBuffer, time);
+  }
 
+  function playNotes() {
+    const speedFactor = 1 - Math.min(currentScore / 50, 0.7);
+    const noteDuration = 0.5 * speedFactor;
+
+    notes.forEach((note, index) => {
+      const time = startTime + index * noteDuration;
+      scheduleNote(note, time);
+    });
+
+    startTime += notes.length * noteDuration;
+  }
+
+  function loop() {
+    playNotes();
+    setTimeout(loop, notes.length * 0.5 * 1000);
+  }
+
+  loop();
+})();
