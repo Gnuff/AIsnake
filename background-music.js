be3d9b6a5731e639bc65f0cd84b00ca81e428367
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 11:11:21 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -1,58 +1,84 @@
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const notes = [261.63, 293.66, 329.63, 392, 440, 493.88, 523.25, 587.33, 659.25];
-let startTime = null;
+
+function playDrumSample(time) {
+  const oscillator = audioContext.createOscillator();
+  const gain = audioContext.createGain();
+
+  oscillator.type = 'sine';
+  oscillator.frequency.setValueAtTime(150, time);
+  oscillator.frequency.exponentialRampToValueAtTime(0.001, time + 0.5);
+
+  gain.gain.setValueAtTime(1, time);
+  gain.gain.exponentialRampToValueAtTime(0.001, time + 0.5);
+
+  oscillator.connect(gain);
+  gain.connect(audioContext.destination);
+
+  oscillator.start(time);
+  oscillator.stop(time + 0.5);
+}
+
+const notes = [
+  110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
+  174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61,
+  130.81, 164.81, 196, 207.65, 196, 164.81, 130.81,
+  207.65, 246.94, 293.66, 311.13, 293.66, 246.94, 207.65
+];
+
+let startTime = audioContext.currentTime + 0.1;
+
 let scheduledNotes = [];
-let timeoutId = null;
-let isPlaying = false;
 
 function scheduleNote(note, time) {
   const osc = audioContext.createOscillator();
-  osc.type = 'sine';
+  osc.type = 'triangle';
   osc.frequency.value = note;
+
   const gain = audioContext.createGain();
   osc.connect(gain);
   gain.connect(audioContext.destination);
+
   const duration = 0.5;
   gain.gain.setValueAtTime(0.4, time);
   gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
+
   osc.start(time);
   osc.stop(time + duration);
+
   scheduledNotes.push({ osc, time, duration });
+
+  playDrumSample(time);
 }
 
 function playNotes() {
   const speedFactor = 1 - Math.min(currentScore / 25, 0.7);
   const noteDuration = 0.5 * speedFactor;
+
   notes.forEach((note, index) => {
     const time = startTime + index * noteDuration;
     scheduleNote(note, time);
   });
+
   startTime += notes.length * noteDuration;
 }
 
 function loop() {
-  if (currentScore >= 2 && !isPlaying) {
-    isPlaying = true;
-    startTime = audioContext.currentTime + 0.1;
-    playNotes();
-  } else if (currentScore < 2 && isPlaying) {
-    isPlaying = false;
-    stopMusic();
-  }
-  timeoutId = setTimeout(loop, (notes.length * 0.5 - 0.1) * 1000);
+  stopMusic();
+  playNotes();
+  setTimeout(loop, notes.length * 0.5 * 1000);
 }
 
 function stopMusic() {
-  scheduledNotes.forEach(({ osc, time, duration }) => {
-    osc.stop(audioContext.currentTime);
+  scheduledNotes.forEach(({osc, time, duration}) => {
+    if (audioContext.currentTime >= time && audioContext.currentTime <= time + duration) {
+      osc.stop(audioContext.currentTime);
+    }
   });
   scheduledNotes = [];
-  startTime = null;
-  clearTimeout(timeoutId);
 }
 
 window.addEventListener('resetMusic', () => {
-  stopMusic();
+  startTime = audioContext.currentTime + 0.1;
 });
 
 loop();
