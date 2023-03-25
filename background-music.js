27759ac6befecabeef1375077c8bd42741a7b393
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 08:18:27 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -14,51 +14,52 @@ function playDrumSample(audioBuffer, time) {
   drumSource.start(time);
 }
 
-const notes = [
-  110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
-  174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61,
-  130.81, 164.81, 196, 207.65, 196, 164.81, 130.81,
-  207.65, 246.94, 293.66, 311.13, 293.66, 246.94, 207.65
-];
+(async () => {
+  const drumBuffer = await loadDrumSample();
+  const notes = [
+    110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
+    174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61,
+    130.81, 164.81, 196, 207.65, 196, 164.81, 130.81,
+    207.65, 246.94, 293.66, 311.13, 293.66, 246.94, 207.65
+  ];
 
-let startTime = audioContext.currentTime + 0.1;
+  let startTime = audioContext.currentTime + 0.1;
 
-function scheduleNote(note, time) {
-  const osc = audioContext.createOscillator();
-  osc.type = 'triangle';
-  osc.frequency.value = note;
+  function scheduleNote(note, time) {
+    const osc = audioContext.createOscillator();
+    osc.type = 'triangle';
+    osc.frequency.value = note;
 
-  const gain = audioContext.createGain();
-  osc.connect(gain);
-  gain.connect(audioContext.destination);
+    const gain = audioContext.createGain();
+    osc.connect(gain);
+    gain.connect(audioContext.destination);
 
-  const duration = 0.5;
-  gain.gain.setValueAtTime(0.4, time);
-  gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
+    const duration = 0.5;
+    gain.gain.setValueAtTime(0.4, time);
+    gain.gain.exponentialRampToValueAtTime(0.001, time + duration);
 
-  osc.start(time);
-  osc.stop(time + duration);
+    osc.start(time);
+    osc.stop(time + duration);
 
-  playDrumSample(drumBuffer, time);
-}
+    playDrumSample(drumBuffer, time);
+  }
 
-function playNotes(currentScore) {
-  const speedFactor = 1 - Math.min(currentScore / 50, 0.7);
-  const noteDuration = 0.5 * speedFactor;
+  function playNotes() {
+    const speedFactor = 1 - Math.min(currentScore / 50, 0.7);
+    const noteDuration = 0.5 * speedFactor;
 
-  notes.forEach((note, index) => {
-    const time = startTime + index * noteDuration;
-    scheduleNote(note, time);
-  });
+    notes.forEach((note, index) => {
+      const time = startTime + index * noteDuration;
+      scheduleNote(note, time);
+    });
 
-  startTime += notes.length * noteDuration;
-}
+    startTime += notes.length * noteDuration;
+  }
 
-// Add currentScore as an argument to the loop function
-function loop(currentScore) {
-  playNotes(currentScore);
-  setTimeout(() => loop(currentScore), notes.length * 0.5 * 1000);
-}
+  window.loop = function () {
+    playNotes();
+    setTimeout(loop, notes.length * 0.5 * 1000);
+  };
 
-// Export the loop function
-export { loop };
+  loop();
+})();
