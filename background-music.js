27d0a219f750103b6f58effa12090e3c65b7b880
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 07:17:53 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -1,32 +1,46 @@
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-function playNote(frequency, startTime, duration) {
+function createOscillator(frequency, type) {
   const oscillator = audioContext.createOscillator();
+  oscillator.type = type || 'sine';
+  oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+  return oscillator;
+}
+
+function createGainNode() {
   const gainNode = audioContext.createGain();
+  gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+  return gainNode;
+}
 
-  oscillator.type = 'triangle';
-  oscillator.frequency.setValueAtTime(frequency, startTime);
-  gainNode.gain.setValueAtTime(1, startTime);
-
-  oscillator.connect(gainNode);
-  gainNode.connect(audioContext.destination);
-
+function playNote(oscillator, gainNode, startTime, duration) {
+  gainNode.gain.setValueAtTime(0, startTime);
+  gainNode.gain.linearRampToValueAtTime(0.5, startTime + 0.01);
+  gainNode.gain.linearRampToValueAtTime(0, startTime + duration - 0.01);
   oscillator.start(startTime);
-  gainNode.gain.exponentialRampToValueAtTime(0.001, startTime + duration);
   oscillator.stop(startTime + duration);
 }
 
 function playMelody() {
-  const notes = [220, 293.66, 329.63, 246.94, 196, 174.61];
-  const duration = 0.5;
+  const notes = [
+    110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
+    174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61,
+  ];
+  const noteDuration = 0.3;
+  const startTime = audioContext.currentTime;
 
-  let startTime = audioContext.currentTime;
+  notes.forEach((note, index) => {
+    const oscillator = createOscillator(note);
+    const gainNode = createGainNode();
 
-  for (let i = 0; i < notes.length; i++) {
-    playNote(notes[i], startTime + i * duration, duration);
-  }
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
 
-  setTimeout(playMelody, notes.length * duration * 1000);
+    const noteStartTime = startTime + noteDuration * index;
+    playNote(oscillator, gainNode, noteStartTime, noteDuration);
+  });
+
+  setTimeout(playMelody, notes.length * noteDuration * 1000);
 }
 
 playMelody();
