41b584f209bf9e80ff7fab840c9421814a56f05c
Gnuff <Gnuff@users.noreply.github.com>
2023-03-25 11:34:27 +0100

    Update background-music.js

--- background-music.js
+++ background-music.js
@@ -1,32 +1,12 @@
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-const notes = [
-  110, 138.59, 164.81, 174.61, 164.81, 138.59, 110,
-  174.61, 207.65, 246.94, 261.63, 246.94, 207.65, 174.61
-];
-
-let startTime = audioContext.currentTime + 0.1;
-let scheduledNotes = [];
-let isMusicPlaying = false; // Add a flag to track if the music is playing
-
-function scheduleNote(note, time) {
-  // ...
-}
-
-function playNotes() {
-  // ...
-}
-
-function stopMusic() {
-  // ...
-}
-
 function loop() {
   if (currentScore === 2 && !isMusicPlaying) {
     isMusicPlaying = true;
     playNotes();
   } else if (currentScore > 2) {
-    playNotes();
+    if (!isMusicPlaying) {
+      isMusicPlaying = true;
+      playNotes();
+    }
   } else if (currentScore === 0 && isMusicPlaying) {
     isMusicPlaying = false;
     stopMusic();
